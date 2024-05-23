50a386febaa3fe7adc127eaf86864719d270c087
Ahmed Esmail <ahmed.esmail.gm@gmail.com>
2024-05-23 16:46:00 +0300

    Domain Unit Test

---
+++ tests/Domain.UnitTests/Candidates/CandidateTests.cs
@@ -0,0 +1,170 @@
+﻿using CandidateManagementSystem.Domain.Entities.Candidates;
+using NUnit.Framework;
+
+namespace CandidateManagementSystem.Domain.UnitTests.Candidates
+{
+    [TestFixture]
+    public class CandidateTests
+    {
+        [Test]
+        public void CreateCandidate_ShouldInitializePropertiesCorrectly()
+        {
+            // Arrange
+            string email = "test@example.com";
+            string firstName = "John";
+            string lastName = "Doe";
+            string phoneNumber = "123-456-7890";
+            TimeSlot? bestTimeToCall = null; // Assume TimeSlot is a valid object
+            string linkedInProfileUrl = "https://linkedin.com/in/johndoe";
+            string gitHubProfileUrl = "https://github.com/johndoe";
+            string comment = "This is a comment";
+
+            // Act
+            var candidate = Candidate.CreateCandidate(
+                email,
+                firstName,
+                lastName,
+                phoneNumber,
+                bestTimeToCall,
+                linkedInProfileUrl,
+                gitHubProfileUrl,
+                comment);
+
+            // Assert
+            Assert.That(candidate.Email, Is.EqualTo(email));
+            Assert.That(candidate.FirstName, Is.EqualTo(firstName));
+            Assert.That(candidate.LastName, Is.EqualTo(lastName));
+            Assert.That(candidate.PhoneNumber, Is.EqualTo(phoneNumber));
+            Assert.That(candidate.BestTimeToCall, Is.EqualTo(bestTimeToCall));
+            Assert.That(candidate.LinkedInProfileUrl, Is.EqualTo(linkedInProfileUrl));
+            Assert.That(candidate.GitHubProfileUrl, Is.EqualTo(gitHubProfileUrl));
+            Assert.That(candidate.Comment, Is.EqualTo(comment));
+        }
+
+        [Test]
+        public void UpdateContactInformation_ShouldUpdatePropertiesCorrectly()
+        {
+            // Arrange
+            var candidate = Candidate.CreateCandidate(
+                "test@example.com",
+                "John",
+                "Doe",
+                "123-456-7890",
+                null,
+                "https://linkedin.com/in/johndoe",
+                "https://github.com/johndoe",
+                "This is a comment");
+
+            string newFirstName = "Jane";
+            string newLastName = "Smith";
+            string newPhoneNumber = "098-765-4321";
+            TimeSlot? newBestTimeToCall = null; // Assume TimeSlot is a valid object
+            string newLinkedInProfileUrl = "https://linkedin.com/in/janesmith";
+            string newGitHubProfileUrl = "https://github.com/janesmith";
+            string newComment = "Updated comment";
+
+            // Act
+            candidate.UpdateContactInformation(
+                newFirstName,
+                newLastName,
+                newPhoneNumber,
+                newBestTimeToCall,
+                newLinkedInProfileUrl,
+                newGitHubProfileUrl,
+                newComment);
+
+            // Assert
+            Assert.That(candidate.FirstName, Is.EqualTo(newFirstName));
+            Assert.That(candidate.LastName, Is.EqualTo(newLastName));
+            Assert.That(candidate.PhoneNumber, Is.EqualTo(newPhoneNumber));
+            Assert.That(candidate.BestTimeToCall, Is.EqualTo(newBestTimeToCall));
+            Assert.That(candidate.LinkedInProfileUrl, Is.EqualTo(newLinkedInProfileUrl));
+            Assert.That(candidate.GitHubProfileUrl, Is.EqualTo(newGitHubProfileUrl));
+            Assert.That(candidate.Comment, Is.EqualTo(newComment));
+        }
+
+        [Test]
+        public void Equals_ShouldReturnTrueForSameEmail()
+        {
+            // Arrange
+            var candidate1 = Candidate.CreateCandidate(
+                "test@example.com",
+                "John",
+                "Doe",
+                "123-456-7890",
+                null,
+                "https://linkedin.com/in/johndoe",
+                "https://github.com/johndoe",
+                "This is a comment");
+
+            var candidate2 = Candidate.CreateCandidate(
+                "test@example.com",
+                "Jane",
+                "Smith",
+                "098-765-4321",
+                null,
+                "https://linkedin.com/in/janesmith",
+                "https://github.com/janesmith",
+                "Another comment");
+
+            // Act
+            bool result = candidate1.Equals(candidate2);
+
+            // Assert
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public void Equals_ShouldReturnFalseForDifferentEmails()
+        {
+            // Arrange
+            var candidate1 = Candidate.CreateCandidate(
+                "test1@example.com",
+                "John",
+                "Doe",
+                "123-456-7890",
+                null,
+                "https://linkedin.com/in/johndoe",
+                "https://github.com/johndoe",
+                "This is a comment");
+
+            var candidate2 = Candidate.CreateCandidate(
+                "test2@example.com",
+                "Jane",
+                "Smith",
+                "098-765-4321",
+                null,
+                "https://linkedin.com/in/janesmith",
+                "https://github.com/janesmith",
+                "Another comment");
+
+            // Act
+            bool result = candidate1.Equals(candidate2);
+
+            // Assert
+            Assert.That(result, Is.False);
+        }
+
+        [Test]
+        public void GetHashCode_ShouldReturnEmailHashCode()
+        {
+            // Arrange
+            string email = "test@example.com";
+            var candidate = Candidate.CreateCandidate(
+                email,
+                "John",
+                "Doe",
+                "123-456-7890",
+                null,
+                "https://linkedin.com/in/johndoe",
+                "https://github.com/johndoe",
+                "This is a comment");
+
+            // Act
+            int hashCode = candidate.GetHashCode();
+
+            // Assert
+            Assert.That(hashCode, Is.EqualTo(email.GetHashCode()));
+        }
+    }
+}
---
+++ tests/Domain.UnitTests/ValueObjects/TimeSlotTests.cs
@@ -0,0 +1,112 @@
+﻿using CandidateManagementSystem.Domain.Entities.Candidates;
+using NUnit.Framework;
+
+namespace CandidateManagementSystem.Domain.UnitTests.ValueObjects
+{
+    [TestFixture]
+    public class TimeSlotTests
+    {
+        [Test]
+        public void TimeSlot_ShouldInitializePropertiesCorrectly()
+        {
+            // Arrange
+            var date = new DateTime(2024, 5, 19);
+            var startTime = new TimeSpan(9, 0, 0);
+            var endTime = new TimeSpan(17, 0, 0);
+
+            // Act
+            var timeSlot = new TimeSlot(date, startTime, endTime);
+
+            // Assert
+            Assert.That(timeSlot.Date, Is.EqualTo(date));
+            Assert.That(timeSlot.StartTime, Is.EqualTo(startTime));
+            Assert.That(timeSlot.EndTime, Is.EqualTo(endTime));
+        }
+
+        [Test]
+        public void TimeSlot_ShouldThrowArgumentException_WhenStartTimeIsAfterEndTime()
+        {
+            // Arrange
+            var date = new DateTime(2024, 5, 19);
+            var startTime = new TimeSpan(17, 0, 0);
+            var endTime = new TimeSpan(9, 0, 0);
+
+            // Act & Assert
+            Assert.Throws<ArgumentException>(() => new TimeSlot(date, startTime, endTime));
+        }
+
+        [Test]
+        public void Equals_ShouldReturnTrueForSameDateAndTimes()
+        {
+            // Arrange
+            var date = new DateTime(2024, 5, 19);
+            var startTime = new TimeSpan(9, 0, 0);
+            var endTime = new TimeSpan(17, 0, 0);
+
+            var timeSlot1 = new TimeSlot(date, startTime, endTime);
+            var timeSlot2 = new TimeSlot(date, startTime, endTime);
+
+            // Act
+            bool result = timeSlot1.Equals(timeSlot2);
+
+            // Assert
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public void Equals_ShouldReturnFalseForDifferentDateOrTimes()
+        {
+            // Arrange
+            var date1 = new DateTime(2024, 5, 19);
+            var date2 = new DateTime(2024, 5, 20);
+            var startTime1 = new TimeSpan(9, 0, 0);
+            var startTime2 = new TimeSpan(10, 0, 0);
+            var endTime1 = new TimeSpan(17, 0, 0);
+            var endTime2 = new TimeSpan(18, 0, 0);
+
+            var timeSlot1 = new TimeSlot(date1, startTime1, endTime1);
+            var timeSlot2 = new TimeSlot(date2, startTime1, endTime1);
+            var timeSlot3 = new TimeSlot(date1, startTime2, endTime1);
+            var timeSlot4 = new TimeSlot(date1, startTime1, endTime2);
+
+            // Act & Assert
+            Assert.That(timeSlot1.Equals(timeSlot2), Is.False);
+            Assert.That(timeSlot1.Equals(timeSlot3), Is.False);
+            Assert.That(timeSlot1.Equals(timeSlot4), Is.False);
+        }
+
+        [Test]
+        public void GetHashCode_ShouldReturnCombinedHashCode()
+        {
+            // Arrange
+            var date = new DateTime(2024, 5, 19);
+            var startTime = new TimeSpan(9, 0, 0);
+            var endTime = new TimeSpan(17, 0, 0);
+
+            var timeSlot = new TimeSlot(date, startTime, endTime);
+
+            // Act
+            int hashCode = timeSlot.GetHashCode();
+
+            // Assert
+            Assert.That(hashCode, Is.EqualTo(HashCode.Combine(date, startTime, endTime)));
+        }
+
+        [Test]
+        public void Create_ShouldReturnNewTimeSlot()
+        {
+            // Arrange
+            var date = new DateTime(2024, 5, 19);
+            var startTime = new TimeSpan(9, 0, 0);
+            var endTime = new TimeSpan(17, 0, 0);
+
+            // Act
+            var timeSlot = TimeSlot.Create(date, startTime, endTime);
+
+            // Assert
+            Assert.That(timeSlot.Date, Is.EqualTo(date));
+            Assert.That(timeSlot.StartTime, Is.EqualTo(startTime));
+            Assert.That(timeSlot.EndTime, Is.EqualTo(endTime));
+        }
+    }
+}
